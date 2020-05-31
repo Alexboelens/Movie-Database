0c6270710b789b6d2d01a37930b53cbb56429a58
Alex Boelens <a.boelens@live.nl>
2020-06-01 01:07:59 +0200

    redux actions refactored

--- client/src/actions/tvShowActions.js
+++ client/src/actions/tvShowActions.js
@@ -4,7 +4,7 @@ const key = '3c29d56bc6a6028be109cd46d895c48e'
 
 
 export const getTrendingTvShows = () => dispatch => {
-    axios.get('https://api.themoviedb.org/3/trending/tv/day?api_key=3c29d56bc6a6028be109cd46d895c48e')
+    axios.get(`https://api.themoviedb.org/3/trending/tv/day?api_key=${key}`)
         .then(res => {
             dispatch({
                 type: GET_ALL_TRENDING_TVSHOWS,
