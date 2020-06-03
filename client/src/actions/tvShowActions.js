9709e57d22a323d3600f483eaf20c53af9a41c76
Alex Boelens <a.boelens@live.nl>
2020-06-03 03:41:24 +0200

    redux updated

--- client/src/actions/tvShowActions.js
+++ client/src/actions/tvShowActions.js
@@ -1,6 +1,15 @@
-import { GET_ALL_TRENDING_TVSHOWS, GET_TVSHOW_BY_ID } from './types';
+import {
+    GET_ALL_TRENDING_TVSHOWS,
+    GET_TVSHOW_BY_ID,
+    GET_TVSHOWS_AIRING_TODAY,
+    GET_TVSHOWS_ON_TV,
+    GET_TVSHOWS_TOP_RATED,
+    GET_TVSHOWS_POPULAR
+} from './types';
+
 import axios from 'axios';
 const key = '3c29d56bc6a6028be109cd46d895c48e'
+
 
 
 export const getTrendingTvShows = () => dispatch => {
@@ -21,6 +30,50 @@ export const getTvShowById = id => dispatch => {
                 type: GET_TVSHOW_BY_ID,
                 payload: res.data,
                 tvShowIsLoaded: true
+            })
+        })
+}
+
+export const getTvShowsAiringToday = page => dispatch => {
+    axios.get(`https://api.themoviedb.org/3/tv/airing_today?api_key=${key}&language=en-US&page=${page}`)
+        .then(res => {
+            dispatch({
+                type: GET_TVSHOWS_AIRING_TODAY,
+                payload: res.data,
+                airingTvShowsLoaded: true
+            })
+        })
+}
+
+export const getTvShowsOnTv = page => dispatch => {
+    axios.get(`https://api.themoviedb.org/3/tv/on_the_air?api_key=${key}&language=en-US&page=${page}`)
+        .then(res => {
+            dispatch({
+                type: GET_TVSHOWS_ON_TV,
+                payload: res.data,
+                TvShowsOnTvLoaded: true
+            })
+        })
+}
+
+export const getPopularTvShows = page => dispatch => {
+    axios.get(`https://api.themoviedb.org/3/tv/popular?api_key=${key}&language=en-US&page=${page}`)
+        .then(res => {
+            dispatch({
+                type: GET_TVSHOWS_POPULAR,
+                payload: res.data,
+                popularTvShowsLoaded: true
+            })
+        })
+}
+
+export const getTopRatedTvShows = page => dispatch => {
+    axios.get(`https://api.themoviedb.org/3/tv/top_rated?api_key=${key}&language=en-US&page=${page}`)
+        .then(res => {
+            dispatch({
+                type: GET_TVSHOWS_TOP_RATED,
+                payload: res.data,
+                topRatedTvShowsLoaded: true
             })
         })
 }
--- client/src/reducers/tvShowReducer.js
+++ client/src/reducers/tvShowReducer.js
@@ -1,10 +1,23 @@
-import { GET_ALL_TRENDING_TVSHOWS, GET_TVSHOW_BY_ID } from '../actions/types';
+import {
+    GET_ALL_TRENDING_TVSHOWS,
+    GET_TVSHOW_BY_ID,
+    GET_TVSHOWS_AIRING_TODAY,
+    GET_TVSHOWS_ON_TV,
+    GET_TVSHOWS_TOP_RATED,
+    GET_TVSHOWS_POPULAR
+} from '../actions/types'
 
 const initialState = {
-    trendingTvShows: '',
-    trendingTvShowsAreLoaded: false,
+    tvShowIsLoaded: false,
     tvShow: '',
-    tvShowIsLoaded: false
+    trendingTvShows: '',
+    tvShowsOnTv: '',
+    popularTvShows: '',
+    tvShowsAiringToday: '',
+    topRatedTvShows: ''
+
+
+
 }
 
 export default function (state = initialState, action) {
@@ -20,6 +33,30 @@ export default function (state = initialState, action) {
                 ...state,
                 tvShow: action.payload,
                 tvShowIsLoaded: action.tvShowIsLoaded
+            }
+        case GET_TVSHOWS_AIRING_TODAY:
+            return {
+                ...state,
+                tvShow: action.payload,
+                airingTvShowsLoaded: action.airingTvShowsLoaded
+            }
+        case GET_TVSHOWS_ON_TV:
+            return {
+                ...state,
+                tvShow: action.payload,
+                TvShowsOnTvLoaded: action.TvShowsOnTvLoaded
+            }
+        case GET_TVSHOWS_POPULAR:
+            return {
+                ...state,
+                tvShow: action.payload,
+                popularTvShowsLoaded: action.popularTvShowsLoaded
+            }
+        case GET_TVSHOWS_TOP_RATED:
+            return {
+                ...state,
+                tvShow: action.payload,
+                topRatedTvShowsLoaded: action.topRatedTvShowsLoaded
             }
         default:
             return state;
