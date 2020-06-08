d1b62cb730072a21461c5d4f9a143aedc8890627
Alex Boelens <a.boelens@live.nl>
2020-06-08 23:51:38 +0200

    create account updated

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -18,10 +18,7 @@ export const loadUser = () => (dispatch, getState) => {
     // user loading
     dispatch({ type: USER_LOADING });
 
-
-
-
-    axios.get('api/auth/user', tokenConfig(getState))
+    axios.get('/api/auth/user', tokenConfig(getState))
         .then(res => dispatch({
             type: USER_LOADED,
             payload: res.data
@@ -33,7 +30,6 @@ export const loadUser = () => (dispatch, getState) => {
             })
         });
 };
-
 
 // setup config headers and token
 export const tokenConfig = getState => {
@@ -54,3 +50,29 @@ export const tokenConfig = getState => {
     }
     return config;
 }
+
+export const register = ({ name, email, password }) => dispatch => {
+    // Headers
+    const config = {
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }
+
+    // request body
+    const body = JSON.stringify({ name, email, password });
+
+    axios.post('/api/users', body, config)
+        .then(res => dispatch({
+            type: REGISTER_SUCCESS,
+            payload: res.data
+        }))
+        .catch(err => {
+            dispatch(returnErrors(err.response.data, err.response.status, 'REGISTER_FAIL'));
+            dispatch({
+                type: REGISTER_FAIL
+            })
+        })
+}
+
+
--- client/src/components/CreateAccount.js
+++ client/src/components/CreateAccount.js
@@ -1,9 +1,122 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Button, Form, FormGroup, Label, Input, Container, Alert } from 'reactstrap';
+import { withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { register } from '../actions/authActions';
 
-const CreateAccount = () => {
+const CreateAccount = ({ history, register, error }) => {
+
+    // useEffect(() => {
+    //     if (error.id === 'REGISTER_FAIL') {
+    //         setState({ msg: error.msg.msg })
+    //         console.log(error)
+    //     } else {
+    //         setState({ msg: null })
+    //         console.log(error)
+    //     }
+    // }, [error])
+
+    const [state, setState] = useState({
+        name: '',
+        email: '',
+        password: '',
+        password2: '',
+        msg: null
+    })
+
+    const { name, email, password, password2, msg } = state;
+
+    const handleChange = e => {
+        setState({
+            ...state,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        const newUser = { name, email, password };
+        console.log(newUser)
+        register(newUser);
+
+    }
+
     return (
-        <h1>Create account page</h1>
+        <Container>
+            <Container className='text-center'>
+                <h3 className='my-5'>Sign up for an account</h3>
+            </Container>
+
+            {msg && <Alert color='danger'>{msg}</Alert>}
+
+            <Form onSubmit={handleSubmit}>
+                <FormGroup>
+                    <Label for="name">Username</Label>
+                    <Input
+                        onChange={handleChange}
+                        value={name}
+                        type="text"
+                        name="name"
+                        id="name"
+                        placeholder="Enter a Username"
+                    />
+                </FormGroup>
+
+                <FormGroup>
+                    <Label for="email">Email</Label>
+                    <Input
+                        onChange={handleChange}
+                        value={email}
+                        type="email"
+                        name="email"
+                        id="email"
+                        placeholder="Enter your email address"
+                    />
+                </FormGroup>
+
+                <FormGroup>
+                    <Label for="password">Password</Label>
+                    <Input
+                        onChange={handleChange}
+                        value={password}
+                        type="password"
+                        name="password"
+                        id="password"
+                        placeholder="Choose a password"
+                    />
+                </FormGroup>
+
+                <FormGroup>
+                    <Label for="password2">Confirm Password</Label>
+                    <Input
+                        onChange={handleChange}
+                        value={password2}
+                        type="password"
+                        name="password2"
+                        id="password2"
+                        placeholder="Re-enter your password"
+                    />
+                </FormGroup>
+
+                <Container className='text-center'>
+                    <Button
+                        color='primary'
+                        type='submit'
+                        className='mt-4'>
+                        Register
+                    </Button>
+                </Container>
+
+            </Form>
+        </Container>
+
     )
 }
 
-export default CreateAccount;
+const mapStateToProps = state => ({
+    isAuthenticated: state.auth.isAuthenticated,
+    error: state.error
+})
+
+
+export default withRouter(connect(mapStateToProps, { register })(CreateAccount));
